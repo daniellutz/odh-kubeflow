5ba8387402d93825a78fc326cbba83496ad9c181
Daniel Lutz <dlutz@redhat.com>
2025-01-13 00:05:18 -0300

    Add methods to handle OAuth client secret generation
    - Add volumes to store the oauth-client configuration;
    - Add extra parameters to container creation, including --client-id, --client-secret and --scope;
    - Add method to generate the secret randomly when authenticating using OAuth;
    - Add link between the notebook's route, user's generated oauth secret and OAuthClient config;

--- components/odh-notebook-controller/controllers/notebook_controller_test.go
+++ components/odh-notebook-controller/controllers/notebook_controller_test.go
@@ -642,6 +642,15 @@ var _ = Describe("The Openshift Notebook controller", func() {
 								},
 							},
 							{
+								Name: "oauth-client",
+								VolumeSource: corev1.VolumeSource{
+									Secret: &corev1.SecretVolumeSource{
+										SecretName:  Name + "-oauth-client",
+										DefaultMode: pointer.Int32Ptr(420),
+									},
+								},
+							},
+							{
 								Name: "tls-certificates",
 								VolumeSource: corev1.VolumeSource{
 									Secret: &corev1.SecretVolumeSource{
@@ -1024,6 +1033,9 @@ func createOAuthContainer(name, namespace string) corev1.Container {
 			"--upstream-ca=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
 			"--email-domain=*",
 			"--skip-provider-button",
+			`--client-id=` + name + `-` + namespace + `-oauth-client`,
+			"--client-secret-file=/etc/oauth/client/secret",
+			"--scope=user:info user:check-access",
 			`--openshift-sar={"verb":"get","resource":"notebooks","resourceAPIGroup":"kubeflow.org",` +
 				`"resourceName":"` + name + `","namespace":"$(NAMESPACE)"}`,
 			"--logout-url=https://example.notebook-url/notebook/" + namespace + "/" + name,
@@ -1072,6 +1084,10 @@ func createOAuthContainer(name, namespace string) corev1.Container {
 			},
 		},
 		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "oauth-client",
+				MountPath: "/etc/oauth/client",
+			},
 			{
 				Name:      "oauth-config",
 				MountPath: "/etc/oauth/config",
